7a987980169a11dc60d0b7543e22b75412ab9898
zero193 <43316929+zero193@users.noreply.github.com>
2018-09-16 18:14:01 +0300

    123

---
+++ hello world.cs
@@ -0,0 +1,13 @@
+using build;
+
+namespace build
+{
+   class build
+   
+    {
+        public static void Main (string[] args )
+        {
+            Console.Witeline ("hello world");
+        }
+    }
+}
